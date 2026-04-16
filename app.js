91ca69454d47dc15e618ba023110396a18933dbf
BotondNovak <novakbotond1294@gmail.com>
2026-04-16 12:48:51 +0200

    Created basic error handling (Add element to table)

---
+++ app.js
@@ -0,0 +1,60 @@
+$(document).ready(function()) {
+
+    $('#add-btn').on('click', function(){
+
+    const id = $('#new-id').val();
+    const name = $('#new-name').val();
+    const type = $('#new-type').val();
+    const mass = $('#new-mass').val();
+    const dilation = $('#new-delation').val();
+    const distance = $('#new-distance').val();
+
+    if (id === ""){
+        alert("Error! You must provide an ID!");
+        $('#new-id').css('border', '2px solid red');
+    } else {
+        $('#new-id').css('border', '1px solid #ccc');
+    }
+    
+    if (name === ""){
+        alert("Error! You must provide an ENTITY name!");
+        $('#new-name').css('border', '2px solid red');
+    } else {
+        $('#new-name').css('border', '1px solid #ccc');
+    }
+
+    if (type === ""){
+        alert("Error! You must provide a TYPE!");
+        $('#new-type').css('border', '2px solid red');
+    } else {
+        $('#new-type').css('border', '1px solid #ccc');
+    }
+    
+    if (mass === ""){
+        alert("Error! You must enter a MASS!");
+        $('#new-mass').css('border', '2px solid red');
+    } else{
+        $('#new-mass').css('border', '1px solid #ccc');
+    }
+    
+    if (dilation === ""){
+        alert("Error! You must enter the TIME Dilation!");
+        $('#new-dilation').css('border', '2px solid red');
+    } else{
+        $('#new-dilation').css('border', '1px solid #ccc');
+    }
+    
+    if (distance === ""){
+        alert("Error! You must enter a DISTANCE!");
+        $('#new-distance').css('border', '2px solid red');
+    } else{
+        $('#new-distance').css('border', '1px solid #ccc');
+    }
+
+
+    
+
+
+
+})
+}
